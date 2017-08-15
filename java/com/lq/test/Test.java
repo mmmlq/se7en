935e0c28f74e67373e44f376352633f643b88096
se7en <52013788@qq.com>
2017-08-15 16:47:28 +0800

    helloworld

---
+++ java/com/lq/test/Test.java
@@ -0,0 +1,10 @@
+package com.lq.test;
+
+/**
+ * Created by Administrator on 2017/8/15 0015.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
